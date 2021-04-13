887211fcdd8902f82251f179d00e7d7816899504
Luan Andrade <luansandrade92@hotmail.com>
2021-04-12 23:18:07 -0300

    create layout with text field and add button

--- lib/home.dart
+++ lib/home.dart
@@ -10,7 +10,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   List _todoList = [];
 
   Future<File> _getFile() async {
@@ -29,13 +28,45 @@ class _HomeState extends State<Home> {
     try {
       final file = await _getFile();
       return file.readAsString();
-    } catch(e) {
+    } catch (e) {
       return null;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Nova tarefa",
+                      labelStyle: TextStyle(color: Colors.deepPurple)
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  child: Text("ADD"),
+                  onPressed: () => debugPrint("ADD"),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.deepPurple
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
