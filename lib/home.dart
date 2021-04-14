eed597df6405c07bcad35d2f71e477381b880a26
Luan Andrade <luansandrade92@hotmail.com>
2021-04-13 23:27:06 -0300

    remove current snackbar to avoid enqueuing snackbars

--- lib/home.dart
+++ lib/home.dart
@@ -168,6 +168,7 @@ class _HomeState extends State<Home> {
               ),
               duration: Duration(seconds: 2),
             );
+            ScaffoldMessenger.of(context).removeCurrentSnackBar();
             ScaffoldMessenger.of(context).showSnackBar(snack);
           });
         },
